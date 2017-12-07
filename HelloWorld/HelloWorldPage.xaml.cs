dd5d8b0703023509842eca73aefacbe1b4347f0b
gregorypaulcrouse <31137876+gregorypaulcrouse@users.noreply.github.com>
2017-12-07 11:43:08 -0500

    First commit

---
+++ HelloWorld/HelloWorldPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class HelloWorldPage : ContentPage
+    {
+        string worldLabelInitialText = " ";
+
+        public HelloWorldPage()
+        {
+            InitializeComponent();
+            worldLabel.Text = worldLabelInitialText;
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            if (worldLabel.Text == worldLabelInitialText){
+                worldLabel.Text = "World";
+            }else{
+                worldLabel.Text = worldLabelInitialText;
+            }
+        }
+    }
+}
